6b7b3400097514fab3d97bf59c6fc6b197abfcc0
satoshi naruki <maptjgpatmpgg1113@gmail.com>
2021-09-18 18:45:40 +0900

    共通化、README追記（Herokuデプロイ手順）

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  let!(:task) { FactoryBot.create(:task) }
+  let!(:second_task) { FactoryBot.create(:second_task) }
+  before do
+    visit tasks_path
+  end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -25,13 +30,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        # テストで使用するためのタスクを作成
-        task = FactoryBot.create(:task, title: 'task')
-        # タスク一覧ページに遷移
-        visit tasks_path
-        # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
-        # have_contentされているか（含まれているか）ということをexpectする（確認・期待する）
-        expect(page).to have_content 'task'
+        expect(page).to have_content 'test_title'
         # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
       end
     end
@@ -45,9 +44,11 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
   end
   describe '詳細表示機能' do
-     context '任意のタスク詳細画面に遷移した場合' do
-       it '該当タスクの内容が表示される' do
-       end
-     end
+    context '任意のタスク詳細画面に遷移した場合' do
+      it '該当タスクの内容が表示される' do
+        visit task_path(id: task.id)
+        expect(page).to have_content 'test_title'
+      end
+    end
   end
 end
