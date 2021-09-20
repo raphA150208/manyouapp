5dcb20ba765fa23558fe4395cbd8b53c3fa60d8f
satoshi naruki <maptjgpatmpgg1113@gmail.com>
2021-09-20 23:03:30 +0900

    テスト（あいまい検索）

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -51,4 +51,28 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
+  describe '検索機能' do
+    before do
+      FactoryBot.create(:task, title: 'task')
+      FactoryBot.create(:second_task, title: 'sample')
+    end
+    context 'タイトルであいまい検索をした場合' do
+      it "検索キーワードを含むタスクで絞り込まれる" do
+        fill_in 'search_title', with: 'task'
+        click_on '検索'
+        expect(page).to have_content 'task'
+      end
+    end
+    context 'ステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        # ここに実装する
+        # プルダウンを選択する「select」について調べてみること
+      end
+    end
+    context 'タイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        # ここに実装する
+      end
+    end
+  end
 end
