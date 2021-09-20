8d87a241723f3c788a7f40a272b09211cb4d8f4e
satoshi naruki <maptjgpatmpgg1113@gmail.com>
2021-09-20 18:22:06 +0900

    ステータス機能追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 20}
+  validates :due_date, presence: true
+  validates :status, presence: true
+  enum status: {未着手:0, 進行中:1, 完了:2}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_19_164538) do
+ActiveRecord::Schema.define(version: 2021_09_20_062944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_09_19_164538) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "due_date", default: -> { "now()" }, null: false
+    t.integer "status", default: 0, null: false
   end
 
 end
