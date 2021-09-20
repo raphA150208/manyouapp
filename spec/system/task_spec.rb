d45047e03e56dd4f111fbe3caca5c74633a79ca4
satoshi naruki <maptjgpatmpgg1113@gmail.com>
2021-09-20 11:25:29 +0900

    終了期限実装

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -8,21 +8,10 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
-      # 1. new_task_pathに遷移する（新規作成ページに遷移する）
-      # ここにnew_task_pathにvisitする処理を書く
       visit new_task_path
-      # 2. 新規登録内容を入力する
-      #「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄にタスクのタイトルと内容をそれぞれ入力する
-      # ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-      # ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
       fill_in 'task[title]', with: 'test_title'
       fill_in 'task[content]', with: 'test_content'
-      # 3. 「登録する」というvalue（表記文字）のあるボタンをクリックする
-      # ここに「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
       click_button '登録する'
-      # 4. clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
-      # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
-      # ここにタスク詳細ページに、テストコードで作成したデータがタスク詳細画面にhave_contentされているか（含まれているか）を確認（期待）するコードを書く
       expect(page).to have_content 'test_title'
       end
     end
@@ -31,7 +20,6 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
         expect(page).to have_content 'test_title'
-        # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
       end
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
